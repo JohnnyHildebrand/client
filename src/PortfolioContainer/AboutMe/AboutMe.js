833252ac6f26c76de4a2a8af120a3019f15007de
Johnny Hildebrand <74218092+JohnnyHildebrand@users.noreply.github.com>
2022-10-20 11:16:47 -0500

    Fix hearder spacing error

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,36 +1,34 @@
-
-import React, { useEffect } from 'react'
-import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
-import ScrollService from '../../utilities/ScrollService'
-import Animations from '../../utilities/Animations'
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
-
 export default function AboutMe(props) {
- let fadeInScreenHandler = (screen)=>{
-    if (screen.fadeScreen !== props.id)
-    return;
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
- };
- const fadeInSubscription =
-  ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  };
+  const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(
+    fadeInScreenHandler
+  );
 
   const Screen_Constants = {
-    description:" Add Blurb about work here",
-    highlights:{
-        bullets:[
-            "Full Stack Web Developer",
-            "Full Stack Web Developer",
-            "Full Stack Web Developer",
-            "Full Stack Web Developer",
-            "Full Stack Web Developer",
-            "end"
-        ],
-        heading:"Here are a few highlights"
+    description: " Add Blurb about work here",
+    highlights: {
+      bullets: [
+        "Full Stack Web Developer",
+        "Full Stack Web Developer",
+        "Full Stack Web Developer",
+        "Full Stack Web Developer",
+        "Full Stack Web Developer",
+        "end",
+      ],
+      heading: "Here are a few highlights",
     },
   };
 
-  const renderHighlights = () => {
+  const renderHighlight = () => {
     return Screen_Constants.highlights.bullets.map((value, i) => (
       <div className="highlight" key={i}>
         <div className="highlight-blob"></div>
@@ -38,33 +36,34 @@ export default function AboutMe(props) {
       </div>
     ));
   };
-    return (
-    <div className="about-me-container screen-container" id={props.id  ||  ""}>
-        <div className="about-me-parent">
-        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me? "}/>
+  return (
+    <div className="about-me-container screen-container " id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
         <div className="about-me-card">
-            <div className="about-me-profile"></div>
-            <div className="about-me-details">
-            <span className="about-me-description">{Screen_Constants.description}
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {Screen_Constants.description}
             </span>
             <div className="about-me-highlights">
-                <div className="about-me-heading">
-                    <span>{Screen_Constants.highlights.heading}</span>
-                </div>
-             {renderHighlights()}
+              <div className="highlight-heading">
+                <span>{Screen_Constants.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
             </div>
             <div className="about-me-options">
-            <button className="btn primary-btn">Hire Me</button>
-            <a
-              href="Resume\Johnny Hildebrand  Updated Resume 2022.pdf"
-              download="Resume Johnny Hildebrand  Updated Resume 2022.pdf"
-            >
+              <button className="btn primary-btn">Hire Me</button>
+              <a
+                href="Resume\Johnny Hildebrand  Updated Resume 2022.pdf"
+                download="Resume Johnny Hildebrand  Updated Resume 2022.pdf"
+              >
               <button className="btn highlighted-btn">Get Resume</button>
-            </a>
+              </a>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
